ea0e2ccd69431a3366e31f7428f2fd7fb5103b5b
weixuan <weixuan@weixuans-MacBook-Pro.local>
2018-11-18 21:36:01 +0800

    initial stage

---
+++ src/containers/BusStopGetter/BusStopGetter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BusStopGetter extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default BusStopGetter;
